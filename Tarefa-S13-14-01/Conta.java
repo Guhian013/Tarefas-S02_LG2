efd4593c7a68fd548fa81363c03fdb3a9d8579f3
Gustavo Ian Gomes do Nascimento <84183686+Guhian013@users.noreply.github.com>
2021-09-15 10:31:22 -0300

    Create Conta.java

---
+++ Tarefa-S13-14-01/Conta.java
@@ -0,0 +1,33 @@
+public class Conta {
+    Boolean teste;
+
+    public int codigo;
+    public double saldo;
+    public double limite = 100.00;
+  
+    public Boolean depositar(double valor) {
+        saldo = saldo + valor;
+        return true;
+    }
+  
+    public Boolean sacar(double valor) {
+        if(saldo >= valor && valor <= limite) {
+            saldo = saldo - valor;
+            teste = true;
+        } else if(saldo < valor && valor > limite) {
+            teste = false;
+        }
+        return teste;
+    }
+  
+    public Boolean transferir(Conta conta, double valor) {
+        if(saldo >= valor && valor <= limite) {
+            saldo = saldo - valor;
+            conta.saldo = conta.saldo + valor;
+            teste = true;
+        } else if(saldo < valor && valor > limite) {
+            teste = false;
+        }
+        return teste;
+    }
+}
